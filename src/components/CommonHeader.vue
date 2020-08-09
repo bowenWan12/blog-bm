861470ce6a662040532566e30fc5b990521f8b72
bowenWan12 <769145832@qq.com>
2020-08-10 00:30:03 +0800

    first commit

---
+++ src/components/CommonHeader.vue
@@ -0,0 +1,79 @@
+<template>
+  <header>
+    <div class="l-content">
+      <el-button type="primary" icon="el-icon-menu" size="mini"></el-button>
+      <el-breadcrumb separator-class="el-icon-arrow-right">
+        <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+        <el-breadcrumb-item :to="current.path" v-if="current">
+          {{current.label}}
+        </el-breadcrumb-item>
+       
+      </el-breadcrumb>
+    </div>
+    <div class="r-content">
+      <el-dropdown trigger="click">
+        <span class="el-dropdown-link">
+          <img :src="userImage" class="user" />
+        </span>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item icon="el-icon-plus">个人中心</el-dropdown-item>
+          <el-dropdown-item icon="el-icon-circle-plus">退出</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+    </div>
+  </header>
+</template>
+
+<script>
+import { mapState } from "vuex";
+export default {
+  computed: {
+     ...mapState({
+      current: state => state.tab.currentMenu
+    })
+  },
+  data() {
+    return {
+      userImage: require("../assets/image/user.png")
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+header {
+  display: flex;
+  height: 100%;
+  align-items: center;
+  justify-content: space-between;
+}
+.l-content {
+  display: flex;
+  align-items: center;
+  .el-button {
+    margin-right: 20px;
+  }
+}
+.r-content {
+  .user {
+    width: 54px;
+    height: 54px;
+    border-radius: 50%;
+  }
+}
+</style>
+<style lang="scss">
+.el-breadcrumb__inner a,
+.el-breadcrumb__inner.is-link {
+  color: #fff;
+}
+
+.el-breadcrumb__item {
+  &:last-child{
+    .el-breadcrumb__inner {
+      color: #f4f4f4;
+    }
+  }
+}
+  
+</style>
---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  devServer: {
+    port: 8888,
+    open: true
+  },
+  css: {
+    loaderOptions: {
+      sass: {
+        prependData: `@import "@/assets/scss/_variable.scss";`
+      }
+    }
+  }
+};
