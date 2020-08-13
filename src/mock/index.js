b886a71c05de3766ba5f70fd1da084eb9a460e31
bowenWan12 <769145832@qq.com>
2020-08-13 17:48:29 +0800

    用户管理页面

--- src/mock/index.js
+++ src/mock/index.js
@@ -1,5 +1,6 @@
 import Mock from 'mockjs'
 import homeApi from './home'
+import userApi from './user'
 
 //设置延时
 Mock.setup({
@@ -8,3 +9,10 @@ Mock.setup({
 
 //首页相关
 Mock.mock(/\/home\/getStatisticalData/, 'get', homeApi.getStatisticalData)
+// 用户相关
+Mock.mock(/\/user\/getUser/, 'get', userApi.getUserList)
+Mock.mock(/\/user\/del/, 'get', userApi.deleteUser)
+Mock.mock(/\/user\/batchremove/, 'get', userApi.batchremove)
+Mock.mock(/\/user\/add/, 'post', userApi.createUser)
+Mock.mock(/\/user\/edit/, 'post', userApi.updateUser)
+Mock.mock(/\/home\/getData/, 'get', homeApi.getStatisticalData)
--- src/views/UserManage/UserManage.vue
+++ src/views/UserManage/UserManage.vue
@@ -1,11 +1,21 @@
 <template>
   <div class="manage">
+    <el-dialog :title="operateType === 'add' ? '新增用户' : '更新用户'" :visible.sync="isShow">
+      <common-from :formLabel="operateLabel" :form="operateFrom">
+
+      </common-from>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="isShow = false">取 消</el-button>
+        <el-button type="primary" @click="confirm">确 定</el-button>
+      </div>
+    </el-dialog>
     <div class="manage-header">
-      <common-from></common-from>
+      <el-button type="primary">+ 新增</el-button>
+      <common-from inline :formLabel="formLabel" :form="serchFrom">
+        <el-button type="primary">搜索</el-button>
+      </common-from>
     </div>
-    <div class="manage-content">
-      <common-table></common-table>
-    </div>
+    <common-table :tableData="tableData" :tableLabel="tableLabel" :config="config" @changePage="getList" @edit="editUser" @del="delUser"></common-table>
   </div>
 </template>
 
@@ -16,7 +26,159 @@ export default {
   components: {
     CommonFrom,
     CommonTable
-  }
+  },
+  data() {
+    return {
+      operateType: 'add',
+      isShow: false,
+      tableData:[],
+      tableLabel: [
+        {
+          prop: "name",
+          label: "姓名"
+        },
+        {
+          prop: "age",
+          label: "年龄"
+        },
+        {
+          prop: "sexLabel",
+          label: "性别",
+          width: "50px"
+        },
+        {
+          prop: "birth",
+          label: "出生日期",
+          width: "200px"
+        },
+        {
+          prop: "addr",
+          label: "地址",
+          width: "300px"
+        }
+      ],
+      config: {
+        page: 1,
+        total: 30,
+        loading: false
+      },
+      operateFrom: {
+        name: '',
+        addr: '',
+        age: '',
+        birth: '',
+        sex: ''
+      },
+      operateLabel: [
+        {
+          model: 'name',
+          label: '姓名',
+        },
+        {
+          model: 'age',
+          label: '年龄',
+        },
+        {
+          model: 'sex',
+          label: '性别',
+          type: 'select',
+          opts: [
+            {
+              label: '男',
+              value: 1
+            },
+            {
+              label: '女',
+              value: 0
+            }
+          ]
+        },
+        {
+          model: 'addr',
+          label: '地址',
+        },
+        {
+          model: 'birth',
+          label: '出生日期',
+          type: 'date'
+        }
+      ],
+      serchFrom: {
+        ketword: ""
+      },
+      formLabel: [
+        {
+          model: "ketword",
+          label: "",
+          options: []
+        }
+      ]
+    }
+  },
+  methods: {
+    getList() {
+      this.config.loading = true
+      this.$http.get("/api/user/getUser", {
+        params: {
+          page: this.config.page
+        }
+      }).then(res => {
+        this.tableData = res.data.list.map(item => {
+          item.sexLabel = item.sex === 0 ? '女' : '男'
+          return item
+        })
+        this.config.total = res.data.count
+        this.config.loading = false
+      })
+    },
+    // changePage(val) {
+    //   console.log(val)
+    // }
+    editUser(row) {
+      this.operateType = 'edit'
+      this.isShow = true
+      this.operateFrom = row
+      console.log(row)
+    },
+    confirm() {
+      if (this.operateType === 'edit') {
+        this.$http.post("/api/user/edit", this.operateFrom).then(res => {
+          console.log(res.data)
+          this.isShow = false
+          this.getList()
+        })
+      }
+    },
+    delUser(row) {
+      this.$confirm('此操作将永久删除该用户, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        let id = row.id
+        this.$http.get("/api/user/del", {
+          params: {
+            id
+          }
+        }).then(res => {
+          console.log(res.data)
+          this.$message({
+            type: 'success',
+            message: '删除成功!'
+          });
+        })
+        
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        });          
+      });
+    }
+  },
+  created () {
+    this.getList();
+  },
 };
 </script>
 
