c423dbc6391bf1f2b7ae5e4232e5adc543779ec7
bowenWan12 <769145832@qq.com>
2020-08-17 17:21:58 +0800

    login page edit

--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -1,27 +1,9 @@
-<!-- 
-<template>
-  <div style="padding: 20px">
-    <el-form :model="form" label-width="120">
-      <el-form-item label="用户名">
-        <el-input v-model="form.username"></el-input>
-      </el-form-item>
-      <el-form-item label="密码">
-        <el-input v-model="form.password" type="password"></el-input>
-      </el-form-item>
-      <el-form-item align="center">
-        <el-button type="primary" @click="login">登录</el-button>
-      </el-form-item>
-    </el-form>
-  </div>
-</template>
--->
-
 <template>
   <div class="login-wrap">
     <div class="ms-login">
-      <div class="ms-title">后台管理系统</div>
+      <div class="ms-title">小轩窗后台管理系统</div>
       <el-form
-        :model="param"
+        :model="form"
         :rules="rules"
         ref="login"
         label-width="0px"
@@ -54,6 +36,10 @@
 export default {
   data() {
     return {
+      rules: {
+          username: [{ required: true, message: '请输入用户名', trigger: 'blur' }],
+          password: [{ required: true, message: '请输入密码', trigger: 'blur' }],
+      },
       form: {
         username: "",
         password: ""
@@ -91,13 +77,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-// .el-form {
-//   width: 50%;
-//   margin: auto;
-//   padding: 45px;
-//   height: 450px;
-//   background-color: #fff;
-// }
 .login-wrap {
   position: relative;
   width: 100%;
